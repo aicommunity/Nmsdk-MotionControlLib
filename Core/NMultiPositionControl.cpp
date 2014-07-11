58a9cbc85b8471abde149010ca08f6350cc21eec
Philip Gundelakh <greatdwarf@yandex.ru>
2014-07-11 17:03:25 +0400

    Fix to NMultiPOsitionControl.

---
+++ Core/NMultiPositionControl.cpp
@@ -0,0 +1,237 @@
+/* ***********************************************************
+
+E-mail:        
+Url:           
+
+This file is part of the project: 
+
+File License:       New BSD License
+Project License:    New BSD License
+See file license.txt for more information
+*********************************************************** */
+
+#ifndef NMultiPositionControl_CPP
+#define NMultiPositionControl_CPP
+
+#include "NMultiPositionControl.h"
+
+namespace NMSDK {
+
+// Методы
+// --------------------------
+// Конструкторы и деструкторы
+// --------------------------
+NMultiPositionControl::NMultiPositionControl(void)
+:
+ PositionControl("PositionControl", this, 0)
+{
+}
+
+NMultiPositionControl::~NMultiPositionControl(void)
+{
+}
+// --------------------------    
+
+
+// ---------------------
+// Методы управления параметрами
+// ---------------------
+// ---------------------
+
+// ---------------------
+// Методы управления переменными состояния
+// ---------------------
+// ---------------------
+
+// --------------------------
+// Системные методы управления объектом
+// --------------------------
+// Выделяет память для новой чистой копии объекта этого класса
+NMultiPositionControl* NMultiPositionControl::New(void)
+{
+ return new NMultiPositionControl;
+}
+// --------------------------
+
+
+// --------------------------
+// Скрытые методы управления счетом 
+// --------------------------
+// Восстановление настроек по умолчанию и сброс процесса счета
+bool NMultiPositionControl::ADefault(void)
+{
+ NPositionControlElement::ADefault();
+ return true;
+}
+
+// Обеспечивает сборку внутренней структуры объекта
+// после настройки параметров
+// Автоматически вызывает метод Reset() и выставляет Ready в true
+// в случае успешной сборки
+bool NMultiPositionControl::ABuild(void)
+{
+ InputNeurons.clear();
+ ControlNeurons.clear();
+ return true;
+}
+
+// Сброс процесса счета без потери настроек
+bool NMultiPositionControl::AReset(void)
+{
+ return true;
+}
+
+// Выполняет расчет этого объекта
+bool NMultiPositionControl::ACalculate(void)
+{
+ ControlNeuronType = "NNewSynSPNeuron";
+ UEPtr<UContainer> cont;
+ UEPtr<UStorage> storage = GetStorage();
+
+ if(InputNeurons.empty()||ControlNeurons.empty())
+   CreateNeurons();
+
+ if(RememberState)
+  {
+   RememberState = false;
+   vector<NNet*> activeInputs, postInputs, preControls, activeControls;
+   for(size_t i=0;i<InputNeurons.size();i++)
+   {
+	UEPtr<UADItem> ltzone=dynamic_pointer_cast<UADItem>(InputNeurons[i]->GetComponentL("LTZone"));
+	if(ltzone->GetOutputData(2).Double[0]>0)
+	{
+	 activeInputs.push_back(InputNeurons[i]);
+	 size_t preControlSize = PreControlNeurons.size();
+	 string preControlNeuronName = "PreControlNeuron"+sntoa(preControlSize+1);
+	 if(CheckComponent(preControlNeuronName))
+	 {
+	  PreControlNeurons.push_back(static_pointer_cast<NNet>(GetComponent(preControlNeuronName)));
+	  preControls.push_back(static_pointer_cast<NNet>(GetComponent(preControlNeuronName)));
+	 }
+	 else
+	 {
+	  cont=dynamic_pointer_cast<UContainer>(storage->TakeObject(ControlNeuronType));
+	  if(!cont)
+	   return 0;
+	  cont->SetName(preControlNeuronName);
+	  AddComponent(cont);
+	  PreControlNeurons.push_back(static_pointer_cast<NNet>(cont));
+	  preControls.push_back(static_pointer_cast<NNet>(cont));
+	 }
+	}
+   }
+   for(size_t c=0;c<PreControlNeurons.size();c++)
+   {
+	UEPtr<UADItem> ltzone=dynamic_pointer_cast<UADItem>(ControlNeurons[c]->GetComponentL("LTZone"));
+	if(ltzone->GetOutputData(2).Double[0]>0)
+	{
+	 activeControls.push_back(ControlNeurons[c]);
+	 size_t postInputSize = PostInputNeurons.size();
+	 string postInputNeuronName = "PostInputNeuron"+sntoa(postInputSize+1);
+	 if(CheckComponent(postInputNeuronName))
+	 {
+	  PostInputNeurons.push_back(static_pointer_cast<NNet>(GetComponent(postInputNeuronName)));
+	  postInputs.push_back(static_pointer_cast<NNet>(GetComponent(postInputNeuronName)));
+	 }
+	 else
+	 {
+	  cont=dynamic_pointer_cast<UContainer>(storage->TakeObject(ControlNeuronType));
+	  if(!cont)
+	   return 0;
+	  cont->SetName(postInputNeuronName);
+	  AddComponent(cont);
+	  PostInputNeurons.push_back(static_pointer_cast<NNet>(cont));
+	  postInputs.push_back(static_pointer_cast<NNet>(cont));
+	 }
+	}
+   }
+   //LinkNegative(activeInputs, postInputs);
+   LinkNeurons(activeInputs, postInputs);
+   LinkNeurons(preControls, activeControls);
+  }
+
+ return true;
+}
+// --------------------------
+
+bool NMultiPositionControl::CreateNeurons(void)
+{
+ UEPtr<UContainer> cont;
+ UEPtr<UStorage> storage = GetStorage();
+ //Creating InputNeurons
+ size_t positionControlSize = PositionControl->size();
+ InputNeuronsByContours.resize(positionControlSize);
+ for(size_t i=0; i<positionControlSize; i++)
+ {
+  for(size_t j=0; j<PositionControl[i]->PostInputNeurons.size(); j++)
+  {
+   UNet *owner=dynamic_pointer_cast<UNet>(GetOwner());
+   string inputNeuronName = "InputNeuron"+sntoa(i+1)+"-"+sntoa(j+1);
+   string ltzoneName, inputName;
+   //UEPtr<UADItem> ltzone=dynamic_pointer_cast<UADItem>(PositionControl[i]->PostInputNeurons[j]->GetComponentL(".LTZone"));
+   PositionControl[i]->PostInputNeurons[j]->GetLongName(owner, ltzoneName);
+   
+   if(CheckComponent(inputNeuronName))
+   {
+	NNet *inputNeuron = static_pointer_cast<NNet>(GetComponent(inputNeuronName));
+	InputNeurons.push_back(inputNeuron);
+	InputNeuronsByContours[i].push_back(inputNeuron);
+	inputNeuron->GetLongName(owner, inputName);
+   }
+   else
+   {
+	InputNeuronType = "NNewSynSPNeuron";
+	cont=dynamic_pointer_cast<UContainer>(storage->TakeObject(InputNeuronType));
+	if(!cont)
+	 return 0;
+	cont->SetName(inputNeuronName);
+	AddComponent(cont);
+	InputNeurons.push_back(static_pointer_cast<NNet>(cont));
+	InputNeuronsByContours[i].push_back(static_pointer_cast<NNet>(cont));
+	cont->GetLongName(owner, inputName);
+   }
+   owner->CreateLink(ltzoneName+".LTZone",0,inputName+".PNeuronMembrane.PosChannel");
+  }
+ }
+ //Creating ControlNeurons 0
+ ControlNeuronsByContours.resize(positionControlSize);
+ for(size_t i=0; i<positionControlSize; i++)
+ {
+  for(size_t j=0; j<PositionControl[i]->PreControlNeurons.size(); j++)
+  {
+   UNet *owner=dynamic_pointer_cast<UNet>(GetOwner());
+   string controlNeuronName = "ControlNeuron"+sntoa(i+1)+"-"+sntoa(j+1);
+   string outputName, controlName;
+   //UEPtr<UADItem> output=dynamic_pointer_cast<UADItem>(PositionControl[i]->PreControlNeurons[j]->GetComponentL(".PNeuronMembrane.PosChannel"));
+   PositionControl[i]->PreControlNeurons[j]->GetLongName(owner, outputName);
+   if(CheckComponent(controlNeuronName))
+   {
+	NNet *controlNeuron = static_pointer_cast<NNet>(GetComponent(controlNeuronName));
+	ControlNeurons.push_back(controlNeuron);
+	ControlNeuronsByContours[i].push_back(controlNeuron);
+	controlNeuron->GetLongName(owner, controlName);
+   }
+   else
+   {
+    ControlNeuronType = "NNewSynSPNeuron";
+	cont=dynamic_pointer_cast<UContainer>(storage->TakeObject(ControlNeuronType));
+	if(!cont)
+	 return 0;
+	cont->SetName(controlNeuronName);
+	AddComponent(cont);
+	ControlNeurons.push_back(static_pointer_cast<NNet>(cont));
+	ControlNeuronsByContours[i].push_back(static_pointer_cast<NNet>(cont));
+	cont->GetLongName(owner, controlName);
+   }
+   owner->CreateLink(controlName+".LTZone",0,outputName+".PNeuronMembrane.PosChannel");
+  }
+ }
+ return true;
+}
+
+
+}
+#endif
+
+
+
---
+++ Core/NMultiPositionControl.h
@@ -0,0 +1,89 @@
+/* ***********************************************************
+
+E-mail:        
+Url:           
+
+This file is part of the project: 
+
+File License:       New BSD License
+Project License:    New BSD License
+See file license.txt for more information
+*********************************************************** */
+#ifndef NMultiPositionControl_H
+#define NMultiPositionControl_H
+
+#include "NPositionControlElement.h"
+
+namespace NMSDK {
+
+class NMultiPositionControl: public NPositionControlElement
+{
+protected: // Свойства
+RDK::UPropertyInputC<NPositionControlElement,NMultiPositionControl> PositionControl;
+
+public: // Переменные состояния
+// Входные нейроны, разделённые по нижним уровням
+// IN(0,0) IN(0,1), ..., IN(0,M) - нейроны, подключенные к первому элементу нижнего уровня
+// IN(1,0) IN(1,1), ..., IN(1,M) - нейроны, подключенные ко второму элементу нижнего уровня
+vector<vector<NNet*> > InputNeuronsByContours;
+// Выходные нейроны, разделённые по нижним уровням
+// Ctrl(0,0) Ctrl(0,1), ..., Ctrl(0,M) - нейроны, подключенные к первому элементу нижнего уровня
+// Ctrl(1,0) Ctrl(1,1), ..., Ctrl(1,M) - нейроны, подключенные ко второму элементу нижнего уровня
+vector<vector<NNet*> > ControlNeuronsByContours;
+
+public: // Методы
+// --------------------------
+// Конструкторы и деструкторы
+// --------------------------
+NMultiPositionControl(void);
+virtual ~NMultiPositionControl(void);
+// --------------------------
+
+// ---------------------
+// Методы управления параметрами
+// ---------------------
+// ---------------------
+
+// ---------------------
+// Методы управления переменными состояния
+// ---------------------
+// ---------------------
+
+// --------------------------
+// Системные методы управления объектом
+// --------------------------
+// Выделяет память для новой чистой копии объекта этого класса
+virtual NMultiPositionControl* New(void);
+// --------------------------        
+
+// --------------------------
+// Скрытые методы управления счетом 
+// --------------------------
+protected:
+// Восстановление настроек по умолчанию и сброс процесса счета
+virtual bool ADefault(void);
+
+// Обеспечивает сборку внутренней структуры объекта
+// после настройки параметров
+// Автоматически вызывает метод Reset() и выставляет Ready в true
+// в случае успешной сборки
+virtual bool ABuild(void);
+
+// Сброс процесса счета без потери настроек
+virtual bool AReset(void);
+
+// Выполняет расчет этого объекта
+virtual bool ACalculate(void);
+// --------------------------
+public:
+bool CreateNeurons(void);
+
+};
+
+
+}
+
+#endif
+
+
+
